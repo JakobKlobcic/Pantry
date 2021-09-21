46a0e0f11bc6c44063528122e947bbd64798073e
JAKOB-LAPTOP\jakob <jakob.klobcic@gmail.com>
2021-09-21 12:40:36 -0600

    Navigation fixed. Navigate using: Navigator.pushNamed(context, "/route", arguments:args)

--- lib/screens/ArticleDetail.dart
+++ lib/screens/ArticleDetail.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import '../models/Article.dart';
 
 class ArticleDetail extends StatelessWidget {
-  var article;
-  ArticleDetail(data){
-    this.article=data;
-  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body:Text("Article detail"));
+    Article article = ModalRoute.of(context)!.settings.arguments as Article;
+
+    return Scaffold(body:Text(article.title));
   }
 
 }
