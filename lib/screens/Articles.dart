cb6569825a022f34ce3c73836f5eba82d92327a2
JAKOB-LAPTOP\jakob <jakob.klobcic@gmail.com>
2021-11-12 20:07:32 -0700

    Article Grouping done. Still need to get page and sort by page

--- lib/screens/Articles.dart
+++ lib/screens/Articles.dart
@@ -2,6 +2,7 @@ import 'package:byu_studies/models/RouteArguments.dart';
 import 'package:byu_studies/widgets/ArticleListItem.dart';
 import 'package:flutter/material.dart';
 import 'package:byu_studies/Widgets/BaseAppBar.dart';
+import 'package:grouped_list/grouped_list.dart';
 
 import '../models/Journal.dart';
 import '../models/Article.dart';
@@ -27,6 +28,23 @@ class Articles extends StatelessWidget {
           Expanded(child:
             FutureBuilder(
               builder: (context, projectSnap) {
+                //TODO: See if there is a way to get the list dynamically
+                List<String> list= [
+                  "Introduction",
+                  "Conference Proceeding",
+                  "Article",
+                  "Document",
+                  "Essay",
+                  "Photo Essay",
+                  "Creative Art",
+                  "Cover Image",
+                  "Poem",
+                  "Review",
+                  "Book Notice",
+                  "Notes and Comments",
+                  "Bibliography",
+                  "Index"
+                ];
                 if (projectSnap.connectionState == ConnectionState.waiting ) {
                   return Center(child: new CircularProgressIndicator());
                 }
@@ -34,19 +52,27 @@ class Articles extends StatelessWidget {
                 return Scaffold(
                   body:Padding(
                     padding: EdgeInsets.all(8.0),
-                    child:ListView.builder(
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        Article article = data[index];
+                    child: GroupedListView<Article, String>(
+                      elements: data,
+                      groupBy: (Article article) => article.type,
+                      groupSeparatorBuilder: (String groupByValue) => Text(groupByValue),
+                      groupComparator: (value1, value2){
+                        var val1Index = list.indexOf(value1);
+                        var val2Index = list.indexOf(value2);
+                        return val1Index.compareTo(val2Index);
+                      },
+                      itemBuilder: (context, article) {
                         return new InkResponse(
                           child:ArticleListItem(article, true),
                           onTap: ()=>{
-                            print(article.title),
                             Navigator.pushNamed(context, "/article_detail", arguments:new Arguments(data:article))
                           },
                         );
                       },
-                    )
+                      //useStickyGroupSeparators: true, // optional
+                      //floatingHeader: true, // optional
+                      order: GroupedListOrder.ASC, // optional
+                    ),
                   ),
                 );
               },
